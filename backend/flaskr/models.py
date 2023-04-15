3cd10f683f688d673d5d32ec7b338bec9f97a574
Mirshod Maxsudov <mirshodmahsudov@gmail.com>
2023-04-15 22:28:54 +0500

    Reformatted according to PEP8

--- backend/flaskr/models.py
+++ backend/flaskr/models.py
@@ -1,10 +1,15 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-import json
+import os
 
-database_name = 'trivia'
-database_path = 'postgresql://mildof: @{}/{}'.format('localhost:5432', database_name)
+
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1:5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_NAME = os.getenv("DB_NAME", "trivia")
+DB_PATH = "postgresql+psycopg2://{}:{}@{}/{}".format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+)
 
 db = SQLAlchemy()
 
@@ -14,7 +19,7 @@ setup_db(app)
 """
 
 
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
@@ -29,7 +34,7 @@ Question
 
 
 class Question(db.Model):
-    __tablename__ = 'questions'
+    __tablename__ = "questions"
 
     id = Column(Integer, primary_key=True)
     question = Column(String)
@@ -54,13 +59,13 @@ class Question(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def format(self):
+    def format(self) -> dict:
         return {
-            'id': self.id,
-            'question': self.question,
-            'answer': self.answer,
-            'category': self.category,
-            'difficulty': self.difficulty
+            "id": self.id,
+            "question": self.question,
+            "answer": self.answer,
+            "category": self.category,
+            "difficulty": self.difficulty,
         }
 
 
@@ -71,7 +76,7 @@ Category
 
 
 class Category(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True)
     type = Column(String)
@@ -80,7 +85,4 @@ class Category(db.Model):
         self.type = type
 
     def format(self):
-        return {
-            'id': self.id,
-            'type': self.type
-        }
+        return {"id": self.id, "type": self.type}
